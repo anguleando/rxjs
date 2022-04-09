ad71eb85b96a39195c1da79c79d2358d314aa816
Juan <jbcorreocontacto@gmail.com>
2022-04-09 10:53:13 +0200

    Add HttpClientModule to app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
@@ -17,6 +18,7 @@ import { OtherComponent } from './components/other/other.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
